398e3cfe43d605242d281ad9b9c4fa6424068109
CMilby <cmilby99@gmail.com>
2019-02-22 09:22:17 -0600

    Added header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import HeaderImg from "./header.png";
+import PersonalImg from "./person.png";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div id="banner">
+          <ul id="banner-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Other</a></li>
+          </ul>
+        </div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={HeaderImg} className="header-back" alt="logo" />
+          <img src={PersonalImg} className="personal-img" />
         </header>
       </div>
     );
