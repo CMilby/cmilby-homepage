9bfaa887db3841ba70b3b9bfa596e0cef99214c6
CMilby <cmilby99@gmail.com>
2019-02-22 09:21:37 -0600

    Updates

---
+++ precache-manifest.393aaaedec8cf3e25f63b7229d1141f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6814d7dc907ab49123c30428b479c04d",
+    "url": "/cmilby-homepage/static/media/person.6814d7dc.png"
+  },
+  {
+    "revision": "ee8ad5d71aaa04b6317659bd763c51d7",
+    "url": "/cmilby-homepage/static/media/header.ee8ad5d7.png"
+  },
+  {
+    "revision": "7c59d92c3fbd2fce7b4f",
+    "url": "/cmilby-homepage/static/js/runtime~main.7c59d92c.js"
+  },
+  {
+    "revision": "0681ce0bff74ce9ea20a",
+    "url": "/cmilby-homepage/static/js/main.0681ce0b.chunk.js"
+  },
+  {
+    "revision": "e2a032349bc2797c4db5",
+    "url": "/cmilby-homepage/static/js/2.e2a03234.chunk.js"
+  },
+  {
+    "revision": "0681ce0bff74ce9ea20a",
+    "url": "/cmilby-homepage/static/css/main.d1388168.chunk.css"
+  },
+  {
+    "revision": "3a80222610649e2819bc6e7cbc0922ff",
+    "url": "/cmilby-homepage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cmilby-homepage/precache-manifest.a8a0cf897e7955eac90d3aef8245cce7.js"
+  "/cmilby-homepage/precache-manifest.393aaaedec8cf3e25f63b7229d1141f7.js"
 );
 
 workbox.clientsClaim();
